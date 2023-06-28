464a0afcec6473d0b4972dd82663ea812cdb1e31
Sm1Ly <5pixeldevelopment@gmail.com>
2023-06-28 17:07:21 +0200

    did some error handling on the server and client side

--- api/src/core/database.ts
+++ api/src/core/database.ts
@@ -1,37 +1,39 @@
-import MySQL, { Pool, PoolOptions, PoolConnection } from "mysql2/promise";
-import { Debug } from "./debug";
+import MySQL, { Pool, PoolOptions, PoolConnection } from 'mysql2/promise';
+import { Debug } from './debug';
 
 const options: PoolOptions = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'countdown_events',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
-}
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'countdown_events',
+	waitForConnections: true,
+	connectionLimit: 10,
+	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	queueLimit: 0,
+	enableKeepAlive: true,
+	keepAliveInitialDelay: 0,
+};
 
 export class Database extends Debug {
+	db: Pool = MySQL.createPool(options);
 
-    db: Pool = MySQL.createPool(options);
+	async query(sql: string, data?: any) {
+		const conn: PoolConnection = await this.clockAsync(
+			'fetching database connection',
+			async () => this.db.getConnection()
+		);
 
-    async query(sql: string, data?: any) {
+		const fsql: string = this.clock('formatting query', () =>
+			conn.format(sql, data)
+		);
 
-        const conn: PoolConnection = await this.clockAsync("fetching database connection", async () => this.db.getConnection());
+		const action = this.timed('executing formatted sql query');
+		const [rows, fields] = await conn.execute(fsql);
+		action();
 
-        const fsql: string = this.clock("formatting query", () => conn.format(sql, data))
+		conn.release();
 
-        const a = this.timed("executing formatted sql query");
-        const [rows, fields] = await conn.execute(fsql)
-        a()
-
-        conn.release()
-
-        return rows;
-    }
-
+		return rows;
+	}
 }
--- countdown/src/core/countdownEventStore.ts
+++ countdown/src/core/countdownEventStore.ts
@@ -2,90 +2,122 @@ import { CountdownEvent } from './countdownEvent';
 import { CountdownEventBuilder } from './countdownEventBuilder';
 
 export class CountdownEventStore {
-    cache = new Map<string, CountdownEvent>();
-    passed = new Set<string>();
-    active = new Set<string>();
+	cache = new Map<string, CountdownEvent>();
+	passed = new Set<string>();
+	active = new Set<string>();
 
-    private readonly loaded: Promise<boolean>;
+	private readonly loaded: Promise<boolean>;
 
-    private async ready(): Promise<boolean> {
-        return await this.loaded;
-    }
+	private async ready(): Promise<boolean> {
+		const isLoaded = await this.loaded;
+		if (isLoaded === false) {
+			console.error(
+				"the countdown event storage couldn't be loaded for some reason..."
+			);
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    private async initialize(): Promise<boolean> {
-        const response = await fetch('http://localhost:3000/api/v1/events', { method: 'GET' });
-        const events = await response.json();
-        if (!events.data) return false;
-        if (events.data.length === 0) return true;
-        await Promise.all(events.data.map(async (
-            data: {
-                uuid: string;
-                name: string;
-                finished_on: string;
-                created_on: string;
-                updated_on: string;
-                deleted_on: string;
-            },
-            index: number
-        ) => {
-            const event = new CountdownEvent(data.uuid, data.name, new Date(data.finished_on));
-            this.cache.set(event.uuid(), event);
-            if (event.finishDate().valueOf() <= Date.now()) {
-                this.passed.add(event.uuid());
-            } else {
-                this.active.add(event.uuid());
-            }
-        }))
-        return true;
-    }
+	private async initialize(): Promise<boolean> {
+		const raw = await fetch('http://localhost:3000/api/v1/events', {
+			method: 'GET',
+		});
 
-    constructor() {
-        this.loaded = this.initialize();
-    }
+		const response = await raw.json();
 
-    async event(uuid: string): Promise<CountdownEvent | null> {
-        if (!this.ready()) return null;
-        if (this.cache.has(uuid)) {
-            return this.cache.get(uuid)!;
-        }
-        return null
-    }
+		if (response.status >= 300) {
+			console.error(response.message);
+			return false;
+		}
 
-    async events(uuids: string[] = [], active: boolean = true): Promise<Array<CountdownEvent> | null> {
-        if (!this.ready()) return null;
-        const events: Array<CountdownEvent> = [];
-        if (uuids.length > 0) {
-            await Promise.all(uuids.map(async (uuid) => {
-                const use = active ? this.active.has(uuid) : this.passed.has(uuid);
-                if (use && this.cache.has(uuid)) {
-                    events.push(this.cache.get(uuid)!);
-                }
-            }));
-        } else {
-            for (const [_, event] of this.cache.entries()) {
-                events.push(event)
-            }
-        }
-        return events;
-    }
+		if (!response.data) return false;
+		if (response.data.length === 0) return true;
 
-    async editEvent(uuid: string): Promise<CountdownEventBuilder | null> {
-        if (!this.ready()) return null;
-        if (uuid && this.cache.has(uuid)) {
-            const event = this.cache.get(uuid)!;
-            return event.edit();
-        }
-        return null;
-    }
+		await Promise.all(
+			response.data.map(
+				async (
+					data: {
+						uuid: string;
+						name: string;
+						finished_on: string;
+						created_on: string;
+						updated_on: string;
+						deleted_on: string;
+					},
+					index: number
+				) => {
+					const event = new CountdownEvent(
+						data.uuid,
+						data.name,
+						new Date(data.finished_on)
+					);
+					this.cache.set(event.uuid(), event);
+					if (event.finishDate().valueOf() <= Date.now()) {
+						this.passed.add(event.uuid());
+					} else {
+						this.active.add(event.uuid());
+					}
+				}
+			)
+		);
+		return true;
+	}
 
-    async deleteEvent(uuid: string): Promise<boolean> {
-        if (!this.ready()) return false;
-        if (uuid && this.cache.has(uuid)) {
-            const event = this.cache.get(uuid)!;
-            event.delete();
-            return true;
-        }
-        return false;
-    }
+	constructor() {
+		this.loaded = this.initialize();
+	}
 
+	async event(uuid: string): Promise<CountdownEvent | null> {
+		if (!this.ready()) return null;
+		if (this.cache.has(uuid)) {
+			return this.cache.get(uuid)!;
+		}
+		return null;
+	}
+
+	async events(
+		uuids: string[] = [],
+		active: boolean = true
+	): Promise<Array<CountdownEvent> | null> {
+		if (!this.ready()) return null;
+		const events: Array<CountdownEvent> = [];
+		if (uuids.length > 0) {
+			await Promise.all(
+				uuids.map(async (uuid) => {
+					const use = active
+						? this.active.has(uuid)
+						: this.passed.has(uuid);
+					if (use && this.cache.has(uuid)) {
+						events.push(this.cache.get(uuid)!);
+					}
+				})
+			);
+		} else {
+			for (const [_, event] of this.cache.entries()) {
+				events.push(event);
+			}
+		}
+		return events;
+	}
+
+	async editEvent(uuid: string): Promise<CountdownEventBuilder | null> {
+		if (!this.ready()) return null;
+		if (uuid && this.cache.has(uuid)) {
+			const event = this.cache.get(uuid)!;
+			return event.edit();
+		}
+		return null;
+	}
+
+	async deleteEvent(uuid: string): Promise<boolean> {
+		if (!this.ready()) return false;
+		if (uuid && this.cache.has(uuid)) {
+			const event = this.cache.get(uuid)!;
+			event.delete();
+			return true;
+		}
+		return false;
+	}
 }
