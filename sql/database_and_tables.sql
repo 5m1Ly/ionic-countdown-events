4a903c373d5cd7a1d4079e1fdc0ef156641e9152
Sm1Ly <5pixeldevelopment@gmail.com>
2023-06-28 17:24:36 +0200

    updated the sql file

--- sql/database_and_tables.sql
+++ sql/database_and_tables.sql
@@ -1,38 +1,40 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               8.0.33 - MySQL Community Server - GPL
+-- Server versie:                10.4.21-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.5.0.6677
+-- HeidiSQL Versie:              11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for countdown_events
-CREATE DATABASE IF NOT EXISTS `countdown_events` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- Databasestructuur van countdown_events wordt geschreven
+DROP DATABASE IF EXISTS `countdown_events`;
+CREATE DATABASE IF NOT EXISTS `countdown_events` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
 USE `countdown_events`;
 
--- Dumping structure for table countdown_events.events
+-- Structuur van  tabel countdown_events.events wordt geschreven
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
-  `uuid` varchar(37) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'UNNAMED EVENT',
-  `finished_on` timestamp NOT NULL DEFAULT (now()),
-  `created_on` timestamp NOT NULL DEFAULT (now()),
-  `updated_on` timestamp NOT NULL DEFAULT (now()),
+  `uuid` varchar(37) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT 'UNNAMED EVENT',
+  `finished_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_on` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`uuid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Dumping data for table countdown_events.events: ~0 rows (approximately)
+-- Dumpen data van tabel countdown_events.events: ~0 rows (ongeveer)
+DELETE FROM `events`;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
